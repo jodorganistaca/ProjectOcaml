898299fb7019f832ce89cc4ce651ad25d7c15f9e
AntoDlmb <git.demo@gmail.com>
2022-01-03 10:42:48 +0100

    debut bipartite

--- src/Bipartite.ml
+++ src/Bipartite.ml
@@ -1,6 +1,7 @@
+open String 
 
-type host = (string, string, string, string, string, string)
-type hacker = (string, string, string, string, string)
+type host = (string*string*string*string*string*string)
+type hacker = (string*string*string*string*string)
 type hosts = host list
 type hackers = hacker list
 
@@ -8,15 +9,15 @@ let all_hackers data_file =
   let rec loop res=
     try
       let line = input_line data_file in 
-      let line = Sring.trim line in
+      let line = String.trim line in
 
       if (line=="")then (loop res)
 
       else
 
-        let h_feature = String.split_on_char " " line in 
-        if (equal (h_feature.[0]) "ha") then
-          let hacker = ((h_feature.[1]),(h_feature.[2]),(h_feature.[3]),(h_feature.[4]),(h_feature.[5]),(h_feature.[6])) in
+        let h_feature = String.split_on_char ' ' line in 
+        if (equal (List.nth h_feature 0) "ha") then
+          let hacker = ((List.nth h_feature 1),(List.nth h_feature 2),(List.nth h_feature 3),(List.nth h_feature 4),(List.nth h_feature 5)) in
           let res = hacker :: res in 
           loop res
 
@@ -26,8 +27,20 @@ let all_hackers data_file =
   in
   loop []
 
+let rec print_list_hackers hackers = 
+  match hackers with 
+  |[]->Printf.printf " \n"
+  |(gender, day, pet, smoke, mixg)::t->Printf.printf "sex : %s, day : %s, pet : %s, smoke : %s, mixg : %s" gender day pet smoke mixg ; print_list_hackers t
 
-let export_to_graph path = ()
+
+let export_to_graph path = 
+  let infile = open_in path in
+  let test = all_hackers infile in
+  print_list_hackers test 
+
+
+
+
 
 
 
